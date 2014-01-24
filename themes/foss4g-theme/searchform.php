f7907d9026762f6d86b90200dfc00949830d1c01
darrell <darrell@renewfund.com>
2014-01-24 15:39:57 -0800

    fix search box for correct website

--- themes/foss4g-theme/searchform.php
+++ themes/foss4g-theme/searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://atlasofdesign.org">
+<form role="search" method="get" id="searchform" class="searchform" action="http://foss4g.renewfund.com">
 	<div>
 		<input type="text" value="" name="s" id="s" placehoder="Search..."/>
 		<input type="submit" id="searchsubmit" value="Search" />
