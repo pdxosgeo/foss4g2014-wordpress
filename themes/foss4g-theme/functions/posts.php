df41f4bed036925fb4a25c68bcc65984471bb5c2
Sam Matthews <matthews.sam@gmail.com>
2014-02-01 15:12:18 -0800

    Update posts.php

--- themes/foss4g-theme/functions/posts.php
+++ themes/foss4g-theme/functions/posts.php
@@ -83,6 +83,6 @@ function create_workshop_post_type() {
 //redirect after post submission
 function custom_redirect( $url ) {
     global $post;
-    return get_permalink( $post->ID=29 ); // needs to be id of specific page (presumably a thankyou page)
+    return get_permalink( $post->ID=184 ); // needs to be id of specific page (presumably a thankyou page)
 }
 add_filter( 'wpuf_after_post_redirect', 'custom_redirect' );
