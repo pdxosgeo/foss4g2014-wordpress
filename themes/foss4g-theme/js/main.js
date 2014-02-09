b609b4678f6d56cbfdf8c4cb2c6f8aa4da377d0d
Sam Matthews <matthews.sam@gmail.com>
2014-02-09 09:43:39 -0800

    new images in js

--- themes/foss4g-theme/js/main.js
+++ themes/foss4g-theme/js/main.js
@@ -1,7 +1,19 @@
 function init() {
 
 	// set image on homepage of portland
-	var homeImages = ['portland-home-1.jpg', 'portland-home-2.jpg'],
+	var homeImages = [
+			'portland-home-1.jpg',
+			'portland-home-2.jpg',
+			'portland-home-3.jpg',
+			'portland-home-4.jpg',
+			'portland-home-5.jpg',
+			'portland-home-6.jpg',
+			'portland-home-7.jpg',
+			'portland-home-8.jpg',
+			'portland-home-9.jpg',
+			'portland-home-10.jpg',
+			'portland-home-11.jpg'
+		],
 		image = Math.floor(Math.random() * homeImages.length);
 	$('#portland-home-image').css('background-image', 'url(wp-content/themes/foss4g-theme/img/portland-home/' + homeImages[image] + ')');
 	console.log(image);
