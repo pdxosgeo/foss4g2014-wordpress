276eacf70a5e18863d4a8b55cb3d08b99c517520
svmatthews <matthews.sam@gmail.com>
2014-02-15 10:10:15 -0800

    some major style updates to page and posts

--- themes/foss4g-theme/page.php
+++ themes/foss4g-theme/page.php
@@ -1,21 +1,67 @@
 <?php get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<section class="page-header">
-	<div class="container">
-		<h1><?php the_title(); ?></h1>
-	</div>
-</section>
-<section class="page-content">
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 content">
-           	<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-        </div>
-        <div class="col-md-4 sidebar">
-        	<?php get_sidebar(); ?>
+
+<section class="page">
+    <div class="container">
+        <div class="row">
+            <!-- left sidebar -->
+            <div class="col-sm-2 left-sidebar">
+            <?php 
+                // page title
+                if($post->post_parent)
+                echo get_the_title($post->post_parent);
+                else
+                the_title();
+
+                // siblings
+                if($post->post_parent)
+                $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+                else
+                $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+                if ($children) { ?>
+                    <ul class="submenu">
+                    <?php echo $children; ?>
+                    </ul>
+            <?php }
+
+                echo 'Sponsors';
+                // 5 random sponsors
+                $args=array(
+                  'post_type' => 'sponsor',
+                  'post_status' => 'publish',
+                  'posts_per_page' => 5,
+                  'orderby' => 'rand'
+                );
+                ?>
+                <ul class="subsponsors">
+                <?php
+                $my_query = null;
+                $my_query = new WP_Query($args);
+                if( $my_query->have_posts() ) {
+                  while ($my_query->have_posts()) : $my_query->the_post(); 
+                    $url = get_post_meta( $post->ID, "_URL", true ); ?>
+                        <li><a href="<?php echo $url; ?>" target="_blank"><?php the_post_thumbnail(); ?></a></li>
+                  <?php endwhile; ?>
+                </ul>
+                <?php }
+                wp_reset_query(); ?>
+            </div>
+
+            <!-- content -->
+            <div class="col-sm-8">
+                <h1 class="title"><?php the_title(); ?></h1>
+                <div class="content">
+                   	<?php the_content(); ?>
+                </div>
+    			<?php endwhile; // end of the loop. ?>
+            </div>
+
+            <!-- right sidebar -->
+            <div class="col-sm-2 visible-lg sidebar">
+            	<?php get_sidebar(); ?>
+            </div>
         </div>
     </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
--- themes/foss4g-theme/single.php
+++ themes/foss4g-theme/single.php
@@ -1,23 +1,48 @@
 <?php get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<section class="page-header post">
-	<div class="container">
-		<h1><?php the_title(); ?></h1>
-	</div>
-</section>
-<section class="page-content">
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 content" id="single-post">
-           	<div class="post-content">
-                <?php the_content(); ?>
+
+<section class="page">
+    <div class="container">
+        <div class="row">
+            <!-- left sidebar -->
+            <div class="col-sm-3 left-sidebar">
+            <?php get_sidebar();
+            
+
+                echo 'Sponsors';
+                // 5 random sponsors
+                $args=array(
+                  'post_type' => 'sponsor',
+                  'post_status' => 'publish',
+                  'posts_per_page' => 5,
+                  'orderby' => 'rand'
+                );
+                ?>
+                <ul class="subsponsors">
+                <?php
+                $my_query = null;
+                $my_query = new WP_Query($args);
+                if( $my_query->have_posts() ) {
+                  while ($my_query->have_posts()) : $my_query->the_post(); 
+                    $url = get_post_meta( $post->ID, "_URL", true ); ?>
+                        <li><a href="<?php echo $url; ?>" target="_blank"><?php the_post_thumbnail(); ?></a></li>
+                  <?php endwhile; ?>
+                </ul>
+                <?php }
+                wp_reset_query(); ?>
             </div>
-			<?php endwhile; // end of the loop. ?>
-        </div>
-        <div class="col-md-4">
-        	<?php get_sidebar(); ?>
+
+            <!-- content -->
+            <div class="col-sm-8">
+                <h1 class="title"><?php the_title(); ?></h1>
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+                <?php endwhile; // end of the loop. ?>
+            </div>
+
         </div>
     </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
