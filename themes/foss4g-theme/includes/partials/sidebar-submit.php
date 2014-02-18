709d3bf8247bdf373b70f10b5321cdab706f36ff
darrell <darrell@renewfund.com>
2014-02-18 10:10:08 -0800

    fix due dates in sidebar

--- themes/foss4g-theme/includes/partials/sidebar-submit.php
+++ themes/foss4g-theme/includes/partials/sidebar-submit.php
@@ -2,5 +2,5 @@
 
 <aside class="sidebar-submit">
 	<h3>Submit</h3>
-	<p>Our submission process is hosted on this site. <a href="/submit-presentation">Presentation proposals</a> and <a href="/ojs/index.php/foss4g2014/user/register">Academic papers</a> are due by <span>March 15th, 2014</span>. <a href="/submit-workshop">Workshop</a> submissions are due by <span>April 15th, 2014.</span></p>
+	<p>Our submission process is hosted on this site. <a href="/submit-presentation">Presentation proposals</a> and <a href="/ojs/index.php/foss4g2014/user/register">Academic papers</a> are due by <span>April 15th, 2014</span>. <a href="/submit-workshop">Workshop</a> submissions are due by <span>March 15th, 2014.</span></p>
 </aside>
