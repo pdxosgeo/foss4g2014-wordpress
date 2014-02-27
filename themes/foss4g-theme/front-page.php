15e9d444bad2bfb003f87eb19036884af2fb4eeb
svmatthews <matthews.sam@gmail.com>
2014-02-26 18:35:02 -0800

    speakers section loops and post types

--- themes/foss4g-theme/front-page.php
+++ themes/foss4g-theme/front-page.php
@@ -83,9 +83,11 @@
       </section>
       <section id="featured-speakers">
         <div class="container">
+          
+          <h1>Speakers</h1>
+          <h4>The best in open geo.</h4>
 
-          <!-- keynote speaker -->
-          <div class="row" id="keynote">
+          <div class="speakers-container">
             <?php
             $args = array(
               'post_type' => 'speaker',
@@ -103,30 +105,29 @@
               if ( $query->have_posts() ) : ?>
                 <!-- the loop -->
                 <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                  <div class="col-xs-6 speaker">
-                    <?php
-                    if (has_post_thumbnail( $post->ID ) ):
-                      $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                      <a href="#"><div class="keynote-photo col-sm-4" style="background-image: url('<?php echo $image[0]; ?>')"></div></a>
-                    <?php endif; ?>
-                    <div class="col-sm-8">
-                      <h2><?php the_title(); ?></h2>
-                      <h3>Company</h3>
-                      <p><?php the_excerpt(); ?></p>
-                    </div>
-                  </div>
+                  <?php
+                  if (has_post_thumbnail( $post->ID ) ):
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                    <a href="#" id="keynote" class="speaker">
+                      <div class="speaker-image" style="background-image: url('<?php echo $image[0]; ?>')"></div>
+                      <div class="name"><?php the_title(); ?></div>
+                     <!--  <div class="speaker-content">
+                        <div class="speaker-group"><?php wp_get_post_terms(); ?></div>
+                        <div class="speaker-name"><?php the_title(); ?></div>
+                        <div class="speaker-info">T</div>
+                      </div> -->
+                      <img id="keynote-bird" src="<?php bloginfo('template_url'); ?>/img/logo_bird.png">
+                      <img id="keynote-callout" src="<?php bloginfo('template_url'); ?>/img/keynote-callout.png">
+                    </a>
+                    
+                  <?php endif; ?>
                 <?php endwhile; ?>
                 <!-- end of the loop -->
                 <?php wp_reset_postdata(); ?>
             <?php else:  ?>
               <p><?php _e( 'No speakers yet!' ); ?></p>
             <?php endif; ?>
-          </div>
-
-          <hr>
-
-          <!-- other featured speakers -->
-          <div class="speakers-container">
+            
             <?php
             $args = array(
               'post_type' => 'speaker',
@@ -148,7 +149,16 @@
                   <?php
                   if (has_post_thumbnail( $post->ID ) ):
                     $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                    <a href="#" class="speaker" style="background-image: url('<?php echo $image[0]; ?>')"></a>
+                    <a href="#" id="keynote" class="speaker">
+                      <div class="speaker-image" style="background-image: url('<?php echo $image[0]; ?>')"></div>
+                      <div class="name"><?php the_title(); ?></div>
+                     <!--  <div class="speaker-content">
+                        <div class="speaker-group"><?php wp_get_post_terms(); ?></div>
+                        <div class="speaker-name"><?php the_title(); ?></div>
+                        <div class="speaker-info">T</div>
+                      </div> -->
+                    </a>
+                    
                   <?php endif; ?>
                 <?php endwhile; ?>
                 <!-- end of the loop -->
