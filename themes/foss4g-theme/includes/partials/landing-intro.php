e84492854fe73894c93b192f1ce94186a1c9cd3f
svmatthews <matthews.sam@gmail.com>
2014-03-09 11:03:38 -0700

    svg scaling and awesomeness

--- themes/foss4g-theme/includes/partials/landing-intro.php
+++ themes/foss4g-theme/includes/partials/landing-intro.php
@@ -4,8 +4,8 @@
     <div class="row">
       <div class="col-sm-3"></div>
   		<div class="col-sm-6" style="text-align:center;">
-  			<img src="<?php bloginfo('template_url'); ?>/img/foss4g-landing-logo.png">
-        <img class="img-responsive" style="max-width:100%;" src="<?php bloginfo('template_url'); ?>/img/foss4g-landing-title.png">
+  			<?php get_template_part( 'includes/partials/inline', 'logo.svg' ); ?>
+        <?php get_template_part( 'includes/partials/inline', 'logotext.svg' ); ?>
         <h3><?php echo get_theme_mod( 'foss4g2014_conference_date' ); ?></h3>
         <p><?php echo get_theme_mod( 'foss4g2014_description' ); ?></p>
       </div>
@@ -15,7 +15,4 @@
     </div>
     
   </div>
-  <!-- <div id="portland">
-    <?php get_template_part( 'includes/partials/inline', 'sthelens.svg' ); ?>
-  </div> -->
 </section>
