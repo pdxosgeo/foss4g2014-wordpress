172896f6afa278f9a15d358e94c557984d7e06d4
darrell <darrell@renewfund.com>
2014-03-13 11:27:47 -0700

    uncomment keynote speaker section

--- themes/foss4g-theme/front-page.php
+++ themes/foss4g-theme/front-page.php
@@ -4,7 +4,7 @@ get_template_part( 'includes/partials/landing-intro', 'landing-intro' );
 get_template_part( 'includes/partials/landing-info', 'landing-info' );
 get_template_part( 'includes/partials/inline', 'downtownportland.svg' );
 get_template_part( 'includes/partials/landing-map', 'landing-map' );           
-// get_template_part( 'includes/partials/landing-keynote', 'landing-keynote' );
+get_template_part( 'includes/partials/landing-keynote', 'landing-keynote' );
 // get_template_part( 'includes/partials/landing-speakers', 'landing-speakers' );
 // get_template_part( 'includes/partials/landing-sponsors', 'landing-sponsors' );
 get_template_part( 'includes/partials/landing-imagebar', 'landing-imagebar' );
