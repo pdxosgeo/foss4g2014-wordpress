561ef8cbf7f5a6a035bd4bc88fafa39f39ef725f
svmatthews <matthews.sam@gmail.com>
2014-04-19 13:41:27 -0700

    map with POIs

--- themes/foss4g-theme/includes/partials/landing-map.php
+++ themes/foss4g-theme/includes/partials/landing-map.php
@@ -1,1 +1,5 @@
-<section id="map"></section>
+<section id="map-container">
+	<ul id="map-nav" class="col-sm-4"></ul>
+	<div id="map" class="col-sm-8"></div>
+	<div id="coordinates"></div>
+</section>
