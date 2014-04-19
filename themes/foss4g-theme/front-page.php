091afce9d6349b172018dedab848a84b2a0d145c
svmatthews <matthews.sam@gmail.com>
2014-04-19 15:59:16 -0700

    get bostock back on the page!

--- themes/foss4g-theme/front-page.php
+++ themes/foss4g-theme/front-page.php
@@ -5,7 +5,7 @@ get_template_part( 'includes/partials/landing-intro', 'landing-intro' );
 // get_template_part( 'includes/partials/landing-reg', 'landing-reg' );
 get_template_part( 'includes/partials/inline', 'downtownportland.svg' );
 get_template_part( 'includes/partials/landing-maininfo', 'landing-maininfo' );
-// get_template_part( 'includes/partials/landing-keynote', 'landing-keynote' );
+get_template_part( 'includes/partials/landing-keynote', 'landing-keynote' );
 get_template_part( 'includes/partials/landing-map', 'landing-map' );           
 // get_template_part( 'includes/partials/landing-speakers', 'landing-speakers' );
 // get_template_part( 'includes/partials/landing-sponsors', 'landing-sponsors' );
--- themes/foss4g-theme/includes/partials/landing-keynote.php
+++ themes/foss4g-theme/includes/partials/landing-keynote.php
@@ -1,13 +1,11 @@
-<section id="main-info">
-    <div class="col-md-12" id="keynote">
-      <img src="<?php bloginfo('theme_url'); ?>/img/mbostock.jpeg" height="100" width="auto">
-      <div class="col-md-6 col-md-offset-6">
-        <h2>Mike Bostock</h2>
-        <p>Mike designs interactive graphics for The New York Times. He is also the author of D3.js, a popular open-source library for visualizing data using web standards, and TopoJSON, an extension to GeoJSON that encodes topology.</p>
-        <a class="btn key">Schedule</a>
-      </div>
+<section id="keynote">
+  <div class="container">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8">
+      <img src="<?php bloginfo('template_url'); ?>/img/mbostock.jpeg">
+      <h2>Mike Bostock <span>KEYNOTE</span></h2>
+      <p>Mike designs interactive graphics for The New York Times. He is also the author of D3.js, a popular open-source library for visualizing data using web standards, and TopoJSON, an extension to GeoJSON that encodes topology.</p>
     </div>
-    <div class="col-md-4">
-      <!-- waka flaka -->
-    </div>
+    <div class="col-sm-2"></div>
+  </div>
 </section>
