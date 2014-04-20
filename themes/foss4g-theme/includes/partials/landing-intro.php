7f6becb728e163fa47509ed2a85c58f218fd9404
svmatthews <matthews.sam@gmail.com>
2014-04-19 19:16:11 -0700

    newsletter link!

--- themes/foss4g-theme/includes/partials/landing-intro.php
+++ themes/foss4g-theme/includes/partials/landing-intro.php
@@ -7,11 +7,10 @@
   			<?php get_template_part( 'includes/partials/inline', 'logo.svg' ); ?>
         <?php get_template_part( 'includes/partials/inline', 'logotext.svg' ); ?>
         <p><span>Portland, Oregon, USA</span> • September 8th - 13th, 2014</p>
-        <a class="btn reg" href="/registration">Register Now</a>        
+        <p id="announcements"><a href="http://eepurl.com/N5Q6X"><span class="glyphicon glyphicon-envelope"></span> Sign up for announcements.</a> </p>
+        <a class="btn reg" href="/registration">Register Now</a>    
       </div>
-      <div class="col-sm-3">
-        
-      </div>
+      <div class="col-sm-3"></div>
     </div>
   </div>
 </section>
