6375064b8e92b6e39116bf4e2f8480d3cc075cea
darrell <darrell@renewfund.com>
2014-04-30 19:02:05 -0700

    remove details on submitting. that ship has sailed

--- themes/foss4g-theme/includes/partials/sidebar-submit.php
+++ themes/foss4g-theme/includes/partials/sidebar-submit.php
@@ -1,7 +1,5 @@
 <?php ?>
 
 <aside class="sidebar-submit">
-	<h3>Submit</h3>
-	<p>Our submission process is hosted on this site. <a href="/submit-presentation">Presentation proposals</a> and <a href="/ojs/index.php/foss4g2014/user/register">Academic papers</a> are due by <span>April 15th, 2014</span>.
-  </p>
+<!-- nothing here right now -->
 </aside>
