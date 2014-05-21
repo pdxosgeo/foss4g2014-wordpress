866d315b8ac2e781c6f526f80614f4f33fdcac19
svmatthews <matthews.sam@gmail.com>
2014-05-21 15:18:44 -0700

    cleaning keynote section

--- themes/foss4g-theme/includes/partials/landing-keynote.php
+++ themes/foss4g-theme/includes/partials/landing-keynote.php
@@ -1,37 +1,35 @@
 <section id="keynote">
-  <div class="container">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-3">
-      <img src="<?php bloginfo('template_url'); ?>/img/mbostock.jpeg">
+  <div class="container" style="max-width:1000px;">
+    <div class="col-xs-4">
+      <img src="<?php bloginfo('template_url'); ?>/img/mbostock.jpeg" style="width:300px;max-width:100%;height:auto;display:block;margin:auto;">
       <h2>Mike Bostock
         <br>
         <small>The New York Times</small>
       </h2>
-      <p>Mike designs interactive graphics for The New York Times. He is also the author of D3.js, a popular open-source library for visualizing data using web standards, and TopoJSON, an extension to GeoJSON that encodes topology.</p>
+      <p class="hidden-sm hidden-xs">Mike designs interactive graphics for The New York Times. He is also the author of D3.js, a popular open-source library for visualizing data using web standards, and TopoJSON, an extension to GeoJSON that encodes topology.</p>
     </div>
-    <div class="col-sm-3">
-      <img src="<?php bloginfo('template_url'); ?>/img/SarahNovotny.jpg">
+    <div class="col-xs-4">
+      <img src="<?php bloginfo('template_url'); ?>/img/SarahNovotny.jpg" style="width:300px;max-width:100%;height:auto;display:block;margin:auto;">
       <h2>Sarah Novotny
         <br>
         <small>NGINX</small>
       </h2>
-      <p>
+      <p class="hidden-sm hidden-xs">
         Sarah Novotny is a technical evangelist and community manager for NGINX.  She has run large scale technology infrastructures as a Systems Engineer and a Database administrator for Amazon.com and the ill fated Ads.com.  In 2001, she founded Blue Gecko, a remote database administration company with two peers from Amazon. Blue Gecko, was sold to DatAvail in 2012.  She’s also curated teams and been a leader in customer communities focused on high availability web application and platform delivery for Meteor Entertainment and Chef.
       </p>
-      <p>
+      <p class="hidden-sm hidden-xs">
         Novotny regularly talks about technology infrastructure and geek lifestyle.  She is also a program chair for O'Reilly Media's OSCON.
       </p>
     </div>
-    <div class="col-sm-3">
-      <img src="<?php bloginfo('template_url'); ?>/img/al_shaw.jpg">
+    <div class="col-xs-4">
+      <img src="<?php bloginfo('template_url'); ?>/img/al_shaw.jpg" style="width:300px;max-width:100%;height:auto;display:block;margin:auto;">
       <h2>Al Shaw
         <br>
         <small>ProPublica</small>
       </h2>
-      <p>
-        Al Shaw is a news applications developer at ProPublica, a non-profit investigative news outlet based in New York City. Equal parts designer, developer and reporter. He has covered campaign finance, schools, disaster recovery and other topics. Before joining ProPublica, Shaw was designer/developer at Talking Points Memo and creator of TPM's popular PollTracker application. Shaw's interactive maps surrounding FEMA's response to Hurricane Sandy was honored with the Society of Professional Journalists' Sigma Delta Chi award. His work has also garnered awards from the Society for News Design, the Online News Association and Investigative Reporters and Editors.
+      <p class="hidden-sm hidden-xs">
+        Al Shaw is a news applications developer at ProPublica, a non-profit investigative news outlet based in New York City. Equal parts designer, developer and reporter, he has covered campaign finance, schools, disaster recovery and other topics. Before joining ProPublica, Shaw was designer/developer at Talking Points Memo and creator of TPM's popular PollTracker application. Shaw's interactive maps surrounding FEMA's response to Hurricane Sandy was honored with the Society of Professional Journalists' Sigma Delta Chi award. His work has also garnered awards from the Society for News Design, the Online News Association and Investigative Reporters and Editors.
       </p>
     </div>
-    <div class="col-sm-2"></div>
   </div>
 </section>
