82323665df74ea98da613a1de611fcfaa5d35d4c
darrell <darrell@renewfund.com>
2014-05-26 11:07:38 -0700

    remove ninja forms, as it is now deprecated by using zendesk directly

--- plugins/ninja-forms/includes/admin/edit-field/remove-button.php
+++
@@ -1,9 +0,0 @@
-<?php
-add_action( 'ninja_forms_edit_field_before_closing_li', 'ninja_forms_edit_field_remove_button' );
-function ninja_forms_edit_field_remove_button( $field_id ){
-	?>
-	<div class="menu-item-actions description-wide submitbox">
-		<a class="submitdelete deletion ninja-forms-field-remove" id="ninja_forms_field_<?php echo $field_id;?>_remove" name="" href="#"><?php _e('Remove', 'ninja-forms'); ?></a>
-	</div>
-	<?php
-}
--- plugins/ninja-forms/includes/display/processing/error-test.php
+++
@@ -1,6 +0,0 @@
-<?php
-//add_action('ninja_forms_post_process', 'ninja_forms_error_test');
-function ninja_forms_error_test(){
-	global $ninja_forms_processing;
-	$ninja_forms_processing->add_error('error_test', 'THIS IS MY ERROR');
-}
