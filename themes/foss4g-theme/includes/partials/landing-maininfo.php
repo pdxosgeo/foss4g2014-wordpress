70254e77547f9e29516dfc2ae69b8ee98abcff7c
darrell <darrell@renewfund.com>
2014-06-04 09:37:35 -0700

    fix typo, add spacing on buttons

--- themes/foss4g-theme/includes/partials/landing-maininfo.php
+++ themes/foss4g-theme/includes/partials/landing-maininfo.php
@@ -16,13 +16,13 @@
         <span class="glyphicon glyphicon-time"></span>
         <h2>Schedule</h2>
         <p class="block-info"><strong>Six</strong> days of presentations, workshops, and great conversation.</p>
-        <a href="https://2014.foss4g.org/schedule/workshops/" class="btn block">Workshops</a>
-        <a href="https://2014.foss4g.org/schedule/sessions/" class="btn block">General Sessions</a>
+        <a href="https://2014.foss4g.org/schedule/workshops/" class="btn block">Workshops</a><br>
+        <a href="https://2014.foss4g.org/schedule/sessions/" style="margin-top: 5px" class="btn block">General Sessions</a>
       </div>
       <div class="col-sm-4 block accomodations">
         <span class="glyphicon glyphicon-plane"></span>
         <h2>Travel & Accomodations</h2>
-        <p class="block-info">Travel grants and information about styaing in Portland.</p>
+        <p class="block-info">Travel grants and information about staying in Portland.</p>
         <a href="https://2014.foss4g.org/attending/accommodations/" class="btn block">Come to FOSS4g!</a>
       </div>
     </div>
