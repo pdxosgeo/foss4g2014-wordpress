3c7915956631a42ba815fa53cef4cf9a1dcb9c12
darrell <darrell@renewfund.com>
2014-08-06 11:16:16 -0700

    update main page to be more relevant to current needs

--- themes/foss4g-theme/includes/partials/landing-maininfo.php
+++ themes/foss4g-theme/includes/partials/landing-maininfo.php
@@ -6,23 +6,18 @@
   </div>
   <div class="container">
     <div class="row">
-      <div class="col-sm-4 block students">
-        <span class="glyphicon glyphicon-bookmark"></span>
-        <h2>Grants & Student Opportunities</h2>
-        <p class="block-info">Travel Grants, discounts, and volunteer opportunites.</p>
-        <a href="https://2014.foss4g.org/attending/discounts-and-grants/" class="btn block">Learn More</a>
-      </div>
-      <div class="col-sm-4 block schedule">
+      <div class="col-sm-6 block schedule">
         <span class="glyphicon glyphicon-time"></span>
         <h2>Schedule</h2>
         <p class="block-info"><strong>Six</strong> days of presentations, workshops, and great conversation.</p>
-        <a href="https://2014.foss4g.org/schedule/workshops/" class="btn block">Workshops</a><br>
+        <a href="https://2014.foss4g.org/schedule/calendar/" class="btn block">Conference Schedule Overview</a><br>
+        <a href="https://2014.foss4g.org/schedule/workshops/" style="margin-top: 5px" class="btn block">Workshops</a><br>
         <a href="https://2014.foss4g.org/schedule/sessions/" style="margin-top: 5px" class="btn block">General Sessions</a>
       </div>
-      <div class="col-sm-4 block accomodations">
+      <div class="col-sm-6 block accomodations">
         <span class="glyphicon glyphicon-plane"></span>
         <h2>Travel & Accomodations</h2>
-        <p class="block-info">Travel grants and information about staying in Portland.</p>
+        <p class="block-info">Information about visitng Portland.</p>
         <a href="https://2014.foss4g.org/attending/accommodations/" class="btn block">Come to FOSS4g!</a>
       </div>
     </div>
