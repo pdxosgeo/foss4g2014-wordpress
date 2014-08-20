65dad64dd7b6a20b3b951cd713240543b9c6c968
darrell <darrell@renewfund.com>
2014-08-20 13:45:49 -0700

    add news link to front page

--- themes/foss4g-theme/includes/partials/landing-intro.php
+++ themes/foss4g-theme/includes/partials/landing-intro.php
@@ -9,6 +9,7 @@
         <p><span>Portland, Oregon, USA</span> • September 8th - 13th, 2014</p>
         <p id="announcements"><a href="http://eepurl.com/N5Q6X"><span class="glyphicon glyphicon-envelope"></span> Sign up for announcements.</a> </p>
         <p id="donations"><a href="http://www.eventbrite.com/e/foss4g-travel-fund-tickets-10102856917"><span class="glyphicon glyphicon-heart-empty"></span>Donate to our Travel Grant Fund.</a> </p>
+        <a class="btn reg" style="margin-bottom: 5px" href="/news">Latest News</a><br>
         <a class="btn reg" href="/registration">Register Now</a>    
         <span> </span>
       </div>
