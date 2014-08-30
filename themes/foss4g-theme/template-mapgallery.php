46e717e2dcee49914a4b89630f75c25954a221b5
Tim Welch <tim.j.welch@gmail.com>
2014-08-30 10:51:05 -0700

    add map shuffle and order param for judging

--- themes/foss4g-theme/template-mapgallery.php
+++ themes/foss4g-theme/template-mapgallery.php
@@ -70,13 +70,34 @@ Template Name: Map Gallery
     </div>
 </div>
 
-<!----- Load the thumbnails ---->
-
 <script type="text/javascript">
+
+//Get parameters from URL
+function getUrlParams() {
+    var result = {};
+    var params = window.location.search.split(/\?|\&/);
+    params.forEach( function(it) {
+        if (it) {
+            var param = it.split("=");
+            result[param[0]] = param[1];
+        }
+    });
+    return result;
+}
+
+//Fisher-Yates-Durstenfeld shuffle
+function shuffle(sourceArray) {
+    for (var n = 0; n < sourceArray.length - 1; n++) {
+        var k = n + Math.floor(Math.random() * (sourceArray.length - n));
+
+        var temp = sourceArray[k];
+        sourceArray[k] = sourceArray[n];
+        sourceArray[n] = temp;
+    }
+}
 
 //Bring back the $ shortcut but just for this page
 var $ = jQuery.noConflict();
-
 //Global variable used by bootstrap-image-gallery.js to lookup map attributes using title as the ID and set other attributes in the modal.  Make the loose assumption that title is unique.  Shameless hack
 maps = null;
 
@@ -87,6 +108,13 @@ jQuery(document).ready(function ($) {
   }).done(function (result) {
     //Set global maps variable to result
     maps = result;
+
+    //fetch URL parameters
+    params = getUrlParams();
+    if (params.order != 'yes') {
+        shuffle(maps);
+    }
+
     var grid = jQuery('#thumb-grid');
     // Add images to grid with attributes to drive modal gallery
     $.each(result, function (index, sub) {
