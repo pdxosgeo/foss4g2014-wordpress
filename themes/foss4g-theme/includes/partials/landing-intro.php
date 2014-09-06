ef546e4e32eac099000be677a2157146dc24aff6
darrell <darrell@renewfund.com>
2014-09-05 23:02:17 -0700

    add a link to the live stream page that goes live on Wednesday

--- themes/foss4g-theme/includes/partials/landing-intro.php
+++ themes/foss4g-theme/includes/partials/landing-intro.php
@@ -10,7 +10,11 @@
         <p id="announcements"><a href="http://eepurl.com/N5Q6X"><span class="glyphicon glyphicon-envelope"></span> Sign up for announcements.</a> </p>
         <p id="donations"><a href="http://www.eventbrite.com/e/foss4g-travel-fund-tickets-10102856917"><span class="glyphicon glyphicon-heart-empty"></span>Donate to our Travel Grant Fund.</a> </p>
         <a class="btn reg" style="margin-bottom: 5px" href="/news">Latest News</a><br>
-        <a class="btn reg" href="/registration">Register Now</a>    
+        <?php 
+          if (date_create('2014-09-10') < time() ) {
+            echo '<a class="btn reg" href="/live">Watch Now!</a>';
+          }
+        ?>
         <span> </span>
       </div>
       <div class="col-sm-3"></div>
