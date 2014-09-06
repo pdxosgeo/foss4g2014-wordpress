aee3e5a2a1e335782ae65672d65acf22013bd9dd
darrell <darrell@renewfund.com>
2014-09-06 13:08:08 -0700

    a few more tweaks to the front buttons.

--- themes/foss4g-theme/includes/partials/landing-intro.php
+++ themes/foss4g-theme/includes/partials/landing-intro.php
@@ -9,12 +9,13 @@
         <p><span>Portland, Oregon, USA</span> • September 8th - 13th, 2014</p>
         <p id="announcements"><a href="http://eepurl.com/N5Q6X"><span class="glyphicon glyphicon-envelope"></span> Sign up for announcements.</a> </p>
         <p id="donations"><a href="http://www.eventbrite.com/e/foss4g-travel-fund-tickets-10102856917"><span class="glyphicon glyphicon-heart-empty"></span>Donate to our Travel Grant Fund.</a> </p>
-        <a class="btn reg" style="margin-bottom: 5px" href="/news">Latest News</a><br>
         <?php 
-          if (date_create('2014-09-10') > time() ) {
-            echo '<a class="btn reg" href="/live">Watch Now!</a>';
+          if (new DateTime('09/10/2014') < new DateTime('now') ) {
+            echo '<a style="margin-bottom: 5px" class="btn-info btn-block btn reg" href="/live">Watch the Live Stream!</a><br>';
           }
         ?>
+        <a class="btn reg  btn-block" style="margin-bottom: 5px" href="/map-gallery">View the Map Gallery</a><br>
+        <a class="btn reg  btn-block" style="margin-bottom: 5px" href="/news">Latest News</a><br>
         <span> </span>
       </div>
       <div class="col-sm-3"></div>
